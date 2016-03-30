60430b35e05330e033f562e5f925bc483080db03
Martin Stancsics <martin.stancsics@gmail.com>
2016-03-30 17:44:24 +0200

    can compute policy function

--- pre-alpha/deaton1991/deaton1991.hs
+++ pre-alpha/deaton1991/deaton1991.hs
@@ -1,10 +1,10 @@
 import Data.List as L
 import Data.Map  as M
 
-type ValueGrid = [Float]
-type PolicyGrid = [Float]
-type Consumption = Float
-type Util = Float
+type ValueGrid = [Double]
+type PolicyGrid = [Double]
+type Consumption = Double
+type Util = Double
 
 -- parameters
 
@@ -36,12 +36,12 @@ pf = ((1+r)*)
 
 -- distance in L inf norm
 
-dist :: ValueGrid -> ValueGrid -> Float
+dist :: ValueGrid -> ValueGrid -> Double
 dist v v' = maximum . L.map abs $ zipWith (\x y -> (x-y)/y) v v'
 
 -- VFI
 
-vfun :: ValueGrid -> Float -> Float
+vfun :: ValueGrid -> Double -> Double
 vfun v = interp mGrid v 
 
 t :: ValueGrid -> ValueGrid
@@ -56,7 +56,7 @@ vList v0 = iterate t v0
 vListDef :: [ValueGrid]
 vListDef = vList $ L.map u mGrid
 
-findv :: Float -> [ValueGrid] -> ValueGrid
+findv :: Double -> [ValueGrid] -> ValueGrid
 findv eps (v1 : v2 : vs) = if   dist v1 v2 < eps
                            then v2
                            else findv eps (v2 : vs)
@@ -64,18 +64,21 @@ findv eps (v1 : v2 : vs) = if   dist v1 v2 < eps
 -- policy function
 
 g :: ValueGrid -> PolicyGrid
-g _ = []
+g v = [maximumC (cGrid k) $ L.map (\c -> (u c) + beta * (evnext k c)) (cGrid k) | k <- mGrid]
+      where evnext k c = sum $ [yProb * (vfun v $ (pf $ k-c) + y) | (y, yProb) <- yProc]
+            cGrid  k = drop 1 $ linspace 0 (pf k) (numC+1)
+            maximumC cGrid l = snd $ maximum $ zip l cGrid
 
 -- helper functions
 
-linspace :: Float -> Float -> Float -> [Float]
+linspace :: Double -> Double -> Double -> [Double]
 linspace a b n = [a, a+(b-a)/(n-1) .. b]
 
-crra :: Float -> Consumption -> Util
+crra :: Double -> Consumption -> Util
 crra 1     c = log c
 crra sigma c = (c ** (1-sigma)) / (1-sigma)
 
-mapInterp :: Map Float Float -> Float -> Float
+mapInterp :: Map Double Double -> Double -> Double
 mapInterp m x = 
     case (M.lookupLE x m, M.lookupGE x m) of
       (Just (a, av), Just (b, bv)) -> 
@@ -87,14 +90,16 @@ mapInterp m x =
       _ -> error "mapInterp"
     where interpolate (a, av) (b, bv) x = av + (x-a) * (bv-av) / (b-a)
     
-interp :: [Float] -> [Float] -> Float -> Float
+interp :: [Double] -> [Double] -> Double -> Double
 interp x0 y0 x = mapInterp (M.fromAscList al) x
                  where al = zip x0 y0
 
 main = do 
     eRaw <- getLine
-    print $ findv (read eRaw :: Float) vListDef
-
+    let v = findv (read eRaw :: Double) vListDef
+    print $ v
+    putStrLn ""
+    print $ g v
 
 
 
