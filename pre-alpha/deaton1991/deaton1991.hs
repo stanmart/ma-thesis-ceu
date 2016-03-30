2cdfc47e55050e9f6d3ee68a1f7e70d4c211530e
Martin Stancsics <martin.stancsics@gmail.com>
2016-03-30 17:22:24 +0200

    Working VFI - same result as matlab

--- pre-alpha/deaton1991/deaton1991.hs
+++ pre-alpha/deaton1991/deaton1991.hs
@@ -1,4 +1,3 @@
--- import Data.Vector as V
 import Data.List as L
 import Data.Map  as M
 
@@ -7,38 +6,74 @@ type PolicyGrid = [Float]
 type Consumption = Float
 type Util = Float
 
-crra :: Float -> Consumption -> Util
-crra sigma c = (c ** (1-sigma)) / (1-sigma)
+-- parameters
 
 u :: Consumption -> Util
-u = crra 3
+u = crra 2
 
-r = 0.05
-beta = 1/1.1
+r      = 0.05
+beta   = 1/1.1
+mu     = 100
+tsigma = 100
+trho   = 0
+
+y = [50, 75, 100, 125, 150]
+yProb = [0.000088417285201, 0.105561356381654, 0.788700452666289, 0.105561356381655, 0.000088417285201]
+yProc = zip y yProb
+
+-- grid
+
+mMin = 20
+mMax = 3*mu
+numM = 30
+mGrid = linspace mMin mMax numM
+numC = 300
+
+-- choice set
+
+gamma k = drop 1 $ linspace 0 (2*mu) (numC+1)
 pf = ((1+r)*)
-grid = [0.01, 0.02 .. 3]
-gamma k = takeWhile (<= (pf k)) grid
+
+-- distance in L inf norm
 
 dist :: ValueGrid -> ValueGrid -> Float
-dist v v' = maximum . L.map abs $ zipWith (-) v v'
+dist v v' = maximum . L.map abs $ zipWith (\x y -> (x-y)/y) v v'
+
+-- VFI
 
 vfun :: ValueGrid -> Float -> Float
-vfun v = interp grid v -- no need for interpolation
-                       -- can only be on grid points
+vfun v = interp mGrid v 
 
 t :: ValueGrid -> ValueGrid
-t v = [maximum $ L.map (\k' -> (f k k') + beta * (vfun v k')) (gamma k) | k <- grid]
-      where f k k' = u $ (pf k) - k
+t v = [maximum $ L.map (\c -> (u c) + beta * (evnext k c)) (cGrid k) | k <- mGrid]
+      where evnext k c = sum $ [yProb * (vfun v $ (pf $ k-c) + y) | (y, yProb) <- yProc]
+            cGrid  k = drop 1 $ linspace 0 (pf k) (numC+1)
+      
 
 vList :: ValueGrid -> [ValueGrid]
 vList v0 = iterate t v0
 
+vListDef :: [ValueGrid]
+vListDef = vList $ L.map u mGrid
+
+findv :: Float -> [ValueGrid] -> ValueGrid
+findv eps (v1 : v2 : vs) = if   dist v1 v2 < eps
+                           then v2
+                           else findv eps (v2 : vs)
+
+-- policy function
+
 g :: ValueGrid -> PolicyGrid
 g _ = []
 
-interp :: [Float] -> [Float] -> Float -> Float
-interp x0 y0 x = mapInterp (M.fromAscList al) x
-                 where al = zip x0 y0
+-- helper functions
+
+linspace :: Float -> Float -> Float -> [Float]
+linspace a b n = [a, a+(b-a)/(n-1) .. b]
+
+crra :: Float -> Consumption -> Util
+crra 1     c = log c
+crra sigma c = (c ** (1-sigma)) / (1-sigma)
 
 mapInterp :: Map Float Float -> Float -> Float
 mapInterp m x = 
@@ -51,9 +86,15 @@ mapInterp m x =
       (Just (a, av), Nothing) -> av
       _ -> error "mapInterp"
     where interpolate (a, av) (b, bv) x = av + (x-a) * (bv-av) / (b-a)
+    
+interp :: [Float] -> [Float] -> Float -> Float
+interp x0 y0 x = mapInterp (M.fromAscList al) x
+                 where al = zip x0 y0
 
-main = print $ v112
-       where v112 = take 1 . drop 111 $ vList grid
+main = do 
+    eRaw <- getLine
+    print $ findv (read eRaw :: Float) vListDef
+
 
 
 
